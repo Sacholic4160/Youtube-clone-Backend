0a6c3b535cb052c5cd49f7febae716ebc1bed269
sacholic4160 <sachinparmar4160@gmail.com>
2024-03-07 15:52:07 +0530

    some new file created and others are modified

---
+++ src/index.js
@@ -0,0 +1,76 @@
+ // (require ('dotenv').config({path: './.env}));   this line of code is used in previous versions so now a days we can resolve it by separating it
+import dotenv from "dotenv";
+// import express from "express"
+// import { DB_NAME}  from "./constants.js"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:"./.env"
+})
+//const app =express();
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//one way to connect our database to server
+
+// //here we can use imediately invoked fxn(iife) and before iife parenthesis we can also use a semicolon to crosscheck so that someone forgot to add a semicolon on the last line.
+
+// ( async ()=> {
+//      try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         //here we check using listeners if our database is connected but it is unable to talk to the express
+//         app.on("error",(err)=>{
+//             console.log(`Error occured :${err}`);
+//             throw err;
+//         })
+        
+//         //here we check if our server is listening on this port or not
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Hii, Server is listening on this port number: ${process.env.PORT}`)
+//         })
+//      } catch (error) {
+//         console.log(`Something went wrong , error occured :${error}`)
+//      }
+// })()
