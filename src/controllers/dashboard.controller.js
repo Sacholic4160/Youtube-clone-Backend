0d7d6b9e9cc30eedab24a80108fc26a6afaaeecd
sacholic4160 <sachinparmar4160@gmail.com>
2024-03-20 18:31:43 +0530

    dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -4,9 +4,13 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js"; 
 import { Like } from "../models/like.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const getChannelStats = asyncHandler(async(req,res) => {
     const { channel } = req.params;
+    // Get the number of subscribers and videos, likes for a given channel
+    
+let stats 
 
     
 })
