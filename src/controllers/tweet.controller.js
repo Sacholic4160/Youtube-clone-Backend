2465b8487e87a01073c146b510765c33acaa1fdf
sacholic4160 <sachinparmar4160@gmail.com>
2024-04-24 18:52:15 +0530

    tweet controller done

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -6,12 +6,11 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-
   //take content of tweet from req.body and user from refreshToken
   const { content } = req.body;
-  if(!content){
-    throw new ApiError(400,"Content is required")
-}
+  if (!content) {
+    throw new ApiError(400, "Content is required");
+  }
   console.log(content);
 
   //take user
@@ -24,16 +23,82 @@ const createTweet = asyncHandler(async (req, res) => {
 
   //create instance
   const tweet = await Tweet.create({
-    content ,
-    owner :user?._id
+    content,
+    owner: user?._id,
+  });
 
-  })
-
-  return res
-  .status(200)
-  .json(200,tweet,"tweet created successfully!!")
-
+  return res.status(200).json(200, tweet, "tweet created successfully!!");
 });
 
+const getUserTweets = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  if (!userId) {
+    throw new ApiError(400, "userId not found!!");
+  }
 
-export {createTweet}
+  const tweets = await Tweet.findById({ owner: userId });
+
+  return res
+    .status(200)
+    .json(200, tweets, "user tweets fetched successfully!!");
+});
+
+const updateTweet = asyncHandler(async (req, res) => {
+  //take content of tweet from req.body and user from refreshToken
+  const { tweetId } = req.params;
+
+  if (!tweetId) {
+    throw new ApiError(400, "tweetId is required!");
+  }
+
+  const user = await User.findOne({
+    refreshToken: req?.cookies?.refreshToken,
+  });
+  if (!user) {
+    throw new ApiError(400, "user is not found!");
+  }
+
+  const tweet = await Tweet.findById({ tweetId });
+  if (!tweet) {
+    throw new ApiError(400, "tweet not found!!");
+  }
+
+  if (tweet.owner.equals(user._id.toString())) {
+    const { content } = req.body;
+    if (!content) {
+      throw new ApiError(400, "content is required!");
+    }
+
+    tweet.content = content;
+    await tweet.save({ validateBeforeSave: false });
+
+    return res.status(200).json(200, tweet, "tweer updated successfully!");
+  } else {
+    throw new ApiError(400, "only the user can update the tweet!");
+  }
+});
+
+const deleteTweet = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+  if (!tweetId) {
+    throw new ApiError(400, "Tweet id cant be fetched for params");
+  }
+  const tweet = await Tweet.findById(tweetId);
+  const user = await User.findOne({
+    refreshToken: req.cookies.refreshToken,
+  });
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  //only the owner can delete the tweet
+  if (tweet?.owner.equals(user._id.toString())) {
+    await Tweet.findByIdAndDelete(tweetId);
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Tweet deleted successfully"));
+  } else {
+    throw new ApiError(401, "Only user can delete the tweet");
+  }
+});
+export { createTweet, updateTweet, getUserTweets, deleteTweet };
--- src/routes/tweet.route.js
+++ src/routes/tweet.route.js
@@ -1,10 +1,21 @@
-import express from "express"
+import express from "express";
 const router = express.Router();
-import { createTweet } from "../controllers/tweet.controller.js";
+import {
+  createTweet,
+  deleteTweet,
+  getUserTweets,
+  updateTweet,
+} from "../controllers/tweet.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-
-//route to post a tweet 
+router.use(verifyJWT);
+//route to post a tweet
 router.route("/create-tweet").post(createTweet);
-
+//route to get all tweets
+router.route("/get-tweets/:userId").get(getUserTweets);
+//router to update a tweet
+router.route("/update-tweet/:tweetId").patch(updateTweet);
+//router to delete a tweet
+router.route("/delete-tweet/:tweetId").delete(deleteTweet);
 
 export default router;
